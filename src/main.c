86cc0a5676c55586fed73863183af17efac9515b
spekkie300 <speklapje300@gmail.com>
2025-02-19 19:02:44 +0100

    Hopefully all finished

--- src/main.c
+++ src/main.c
@@ -1,14 +1,30 @@
+#include <fcntl.h>
+#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/termios.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // function declations
 uint16_t sign_extend(uint16_t x, int bit_count);
-uint16_t update_flags(uint16_t r);
+void update_flags(uint16_t r);
+int read_image(const char* image_path);
+void read_image_file(FILE* file);
+uint16_t swap16(uint16_t x);
+void mem_write(uint16_t address, uint16_t val);
+uint16_t mem_read(uint16_t address);
+void handle_interrupt(int signal);
 
 // Memory definitions and declaration
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];
+
+// Memory mapped registers
+enum { MR_KBSR = 0xFE00, MR_KBDR = 0xFE02 };
 
 // Registers
 enum {
@@ -66,6 +82,30 @@ enum {
   TRAP_HALT = 0x25,   // halt the program
 };
 
+struct termios original_tio;
+
+void disable_input_buffering() {
+  tcgetattr(STDIN_FILENO, &original_tio);
+  struct termios new_tio = original_tio;
+  new_tio.c_lflag &= ~ICANON & ~ECHO;
+  tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering() {
+  tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key() {
+  fd_set readfds;
+  FD_ZERO(&readfds);
+  FD_SET(STDIN_FILENO, &readfds);
+
+  struct timeval timeout;
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 0;
+  return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
 int main(int argc, const char* argv[]) {
   if (argc < 2) {
     printf("Usage: ./lc3 [image-file]... \n");
@@ -73,7 +113,15 @@ int main(int argc, const char* argv[]) {
   }
 
   // for loop to read the image-file
-  // TODO
+  for (int j = 1; j < argc; ++j) {
+    if (!read_image(argv[j])) {
+      printf("failed to load image: %s\n", argv[j]);
+      return 1;
+    }
+  }
+
+  signal(SIGINT, handle_interrupt);
+  disable_input_buffering();
 
   // Set the Z flag since COND flag needs to be set
   reg[R_COND] = FL_ZRO;
@@ -264,6 +312,7 @@ int main(int argc, const char* argv[]) {
     }
   }
   // shutdown function
+  restore_input_buffering();
 }
 
 uint16_t sign_extend(uint16_t x, int bit_count) {
@@ -273,7 +322,7 @@ uint16_t sign_extend(uint16_t x, int bit_count) {
   return x;
 }
 
-uint16_t update_flags(uint16_t r) {
+void update_flags(uint16_t r) {
   if (reg[r] == 0) {
     reg[R_COND] = FL_ZRO;
   } else if (reg[r] >>
@@ -282,4 +331,52 @@ uint16_t update_flags(uint16_t r) {
   } else {
     reg[R_COND] = FL_POS;
   }
+}
+
+void read_image_file(FILE* file) {
+  uint16_t origin;
+  fread(&origin, sizeof(origin), 1, file);
+  origin = swap16(origin);
+
+  uint16_t max_read = MEMORY_MAX - origin;
+  uint16_t* p = memory + origin;
+  size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+  while (read-- > 0) {
+    *p = swap16(*p);
+    ++p;
+  }
+}
+
+uint16_t swap16(uint16_t x) { return (x << 8) | (x >> 8); }
+
+int read_image(const char* image_path) {
+  FILE* file = fopen(image_path, "rb");
+  if (!file) {
+    return 0;
+  };
+  read_image_file(file);
+  fclose(file);
+  return 1;
+}
+
+void mem_write(uint16_t address, uint16_t val) { memory[address] = val; }
+// TODO
+
+uint16_t mem_read(uint16_t address) {
+  if (address == MR_KBSR) {
+    if (check_key()) {
+      memory[MR_KBSR] = (1 << 15);
+      memory[MR_KBDR] = getchar();
+    } else {
+      memory[MR_KBSR] = 0;
+    }
+  }
+  return memory[address];
+}
+
+void handle_interrupt(int signal) {
+  restore_input_buffering();
+  printf("\n");
+  exit(-2);
 }
